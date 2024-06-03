3d96674a7d31032be5174ee2ee15fc1c5d10e8b0
kamiji-tomoyuki <k023g0115@g.neec.ac.jp>
2024-06-03 12:58:03 +0900

    地面との衝突判定

--- Assets/PullingJump.cs
+++ Assets/PullingJump.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PullingJump : MonoBehaviour
 {
     private Vector3 clickPosition;
+    private bool isCanJump;
 
     [SerializeField]
     private float jumpPower = 20;
@@ -24,7 +25,7 @@ public class PullingJump : MonoBehaviour
         {
             clickPosition = Input.mousePosition;
         }
-        if (Input.GetMouseButtonUp(0))
+        if (isCanJump && Input.GetMouseButtonUp(0))
         {
             //クリックした座標と離した座標の差分を取得
             Vector3 dist = clickPosition-Input.mousePosition;
@@ -38,5 +39,36 @@ public class PullingJump : MonoBehaviour
         {
             Physics.gravity *= -1;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("衝突した");
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("接触中");
+        //衝突している点の情報が複数格納されている
+        ContactPoint[] contacts = collision.contacts;
+        //0番目の衝突情報から、衝突している点の法線を取得
+        Vector3 otherNormal = contacts[0].normal;
+        //上方向ベクトル 長さ1
+        Vector3 upVector = new Vector3(0,1,0);
+        //上方向と法線の内積
+        float dotUN = Vector3.Dot(upVector, otherNormal);
+        //内積値に逆三角関数arccosをかけて角度を算出、度数法に変換
+        float dotDeg = Mathf.Acos(dotUN) * Mathf.Rad2Deg;
+        //2つのベクトルがなす角度が45度より小さければジャンプ可能
+        if( dotDeg <= 45 )
+        {
+            isCanJump = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("離脱した");
+        isCanJump = false;
     }
 }
